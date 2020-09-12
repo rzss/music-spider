995bc0ae4711fd59daf366e8e45ff2d435a0a5ae
rzss <rzsssince@163.com>
2020-09-12 23:06:10 +0800

    first try

---
+++ main.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###
+# File: e:\code\python\spider\.vscode\music_spider\main.py
+# Project: e:\code\python\spider\.vscode\music_spider
+# Created Date: Saturday, September 12th 2020, 10:10:32 pm
+# Author: rz
+# -----
+# Last Modified:
+# Modified By:
+# -----
+# Copyright (c) 2020 rz WTI bupt
+#
+# All shall be well and all shall be well and all manner of things shall be well.
+# Nope...we're doomed!
+# -----
+# HISTORY:
+# Date      	By	Comments
+# ----------	---	----------------------------------------------------------
+###
